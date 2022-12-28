dc8f9365965140e2ee17857771afd1c4ab0bca23
Hiro Mia <hiromia006@gmail.com>
2022-12-28 22:18:11 +0600

    first commit

---
+++ src/test/java/batch4/GeckoDriverTest.java
@@ -0,0 +1,21 @@
+package batch4;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chromium.ChromiumDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class GeckoDriverTest {
+    public static void main(String[] args) throws InterruptedException {
+//        System.setProperty("webdriver.gecko.driver", "/home/jannat/githubProjects/seleniumBatch4/drivers/geckodriver");
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://www.selenium.dev/");
+        Thread.sleep(4000);
+
+        driver.quit();
+    }
+}
