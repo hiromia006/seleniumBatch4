2d384dea1d838b0b1f660d138cbba36625fe8208
Hiro Mia <hiromia006@gmail.com>
2022-12-31 22:50:18 +0600

    Selenium batch 4 Selector/Locator

--- src/test/java/batch4/class2/NameSelectorTest.java
+++ src/test/java/batch4/class2/NameSelectorTest.java
@@ -13,21 +13,31 @@ public class NameSelectorTest {
         driver.get("https://parabank.parasoft.com/parabank");
         driver.manage().window().maximize();
 
+        // name locator/Selector
         driver.findElement(By.name("username")).sendKeys("laskjdklasjd");
         Thread.sleep(2000);
         driver.findElement(By.name("password")).sendKeys("laskjdklasjd");
         Thread.sleep(2000);
 
+        // linkText locator/Selector
         driver.findElement(By.linkText("Services")).click();
         Thread.sleep(2000);
 
+        // partialLinkText locator/Selector
         driver.findElement(By.partialLinkText("min Pa")).click();
         Thread.sleep(2000);
 
+        // tagName locator/Selector
         driver.findElement(By.tagName("div")).click();
         Thread.sleep(2000);
 
+        // absolute xpath locator/Selector
+        driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[1]/div/form/div[1]/input")).sendKeys("asdsadsad");
+        Thread.sleep(2000);
 
+        // Relative xpath locator/Selector
+        driver.findElement(By.xpath("//input[@name='password1']")).sendKeys("asdksa");
+        Thread.sleep(2000);
 
         driver.quit();
     }
